92f2b1cd9debf88fee33dbd5a124caf3f8987a34
Gede Yoga <permana0912@gmail.com>
2024-03-17 03:17:06 +0800

    chage permission menu

--- resources/js/components/core/Admin.vue
+++ resources/js/components/core/Admin.vue
@@ -12,11 +12,23 @@
                         <div class="container-xxl flex-grow-1 container-p-y">
                            <router-view></router-view>
                         </div>
+                        <footer class="content-footer footer bg-footer-theme">
+                            <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                                <div class="mb-2 mb-md-0">
+                                    ©
+                                    Copyright by
+                                    <a href="#" target="_blank" class="footer-link fw-bolder">Samartha</a>
+                                </div>
+                                <!-- <div>
+                                    <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                                    <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
 
+                                    <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
 
-                        <div>
-                            dsfnsdkfdsk
-                        </div>
+                                    <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank" class="footer-link me-4">Support</a>
+                                </div> -->
+                            </div>
+                        </footer>
                     </div>
                 </div>
             </div>
